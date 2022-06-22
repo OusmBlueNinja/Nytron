74dc4321f399e767fbbd39c14f80cdb139ce664f
Gigabite Studio's LLC <89956790+OusmBlueNinja@users.noreply.github.com>
2022-06-22 03:18:29 -0500

    Wall Jumps

--- Nytron.py
+++ Nytron.py
@@ -1,3 +1,4 @@
+from msilib import CAB
 import os,time, pygame
 
 pygame.init()
@@ -16,13 +17,14 @@ font = pygame.font.Font(None, 25)
 width = 1280
 height = 720
 cooldown = 0
+jump = False
 
 # Create the clock object
 clock = pygame.time.Clock()
 logo = pygame.image.load("assets/Nytron.png")
 playerlgo = pygame.image.load("assets/Player.png")
 win = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Nytron Alpha 000.000.002")
+pygame.display.set_caption("Nytron Alpha 000.000.003")
 pygame.display.set_icon(logo)
 
 
@@ -32,13 +34,14 @@ class Player:
     self.x = width/2
     self.y = height/2
     self.speedx = 0
-    self.vel = 1.5
+    self.xvel = 1.5
+    self.yvel = 3
     self.grav_vel = 0.5
     self.size = 64
     self.bottom = 656.5
   
   def gravity(self):
-    if self.y <= height - player.size :
+    if self.y <= height - player.size:
       self.y += self.grav_vel
 
 player = Player()
@@ -51,6 +54,7 @@ playerIcon = pygame.transform.scale(playerlgo, (player.size, player.size))
 run = True
 movey = 0
 movex = 0
+movey2 = 0
 while run:
     win.fill(sky_blue)
     framerate = round(clock.get_fps(), 1)
@@ -65,6 +69,13 @@ while run:
         if event.type == pygame.QUIT:
             run = False
         keys = pygame.key.get_pressed()
+        
+        if keys[pygame.K_UP]:
+          if cooldown == 0:
+            if player.y == player.bottom:
+              movey = 1
+              cooldown = 300
+              jump = True
     
         if keys[pygame.K_LEFT]:
             movex = -1
@@ -72,22 +83,37 @@ while run:
             movex = 1
         else:
           movex = 0
-        if keys[pygame.K_UP]:
-          if cooldown == 0:
-            if player.y == player.bottom:
-              movey = 1
-              cooldown = 300
+        
+        if player.x == -0.5:
+          if keys[pygame.K_UP]:
+            movey2 = 1
+            can_jump = False
+      
+          
+        
         
     
     if movex == 1 and player.x < width - player.size:
-      player.x += player.vel
+      player.x += player.xvel
     elif movex == -1 and player.x > 0:
-      player.x -= player.vel
-    elif movey == 1 and cooldown >= 100:
-      player.y -= player.vel
+      player.x -= player.xvel
+    if movey == 1 and cooldown >= 200:
+      player.y -= player.yvel
+      can_jump = False
+    if movey2 == 1:
+      player.y -= player.yvel
     if player.y == player.bottom:
+      jump = False
       movey = 0
+      movey2 = 0
+      
+
+    
+    
+    
         
+        
+    
           
     if cooldown != 0:
       cooldown -= 1   
